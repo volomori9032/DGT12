f2e85e1a1ee543ece090c58007df03ea5402bb03
meow meow <scarycat105@gmail.com>
2025-03-14 11:46:35 +1300

    Update test5(coding).py

--- tests/test5(coding).py
+++ tests/test5(coding).py
@@ -1,1 +1,9 @@
+# importing easygui module
 from easygui import *
+ 
+username = enterbox("Please enter your choice of identifcation: ")
+if username:
+    msgbox(f"Your selected username is {username}")
+
+else:
+    msgbox("No username entered.")
